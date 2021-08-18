7c6b2347f561bd6ccc726b0c25ddb6a42d102f7d
Nathan Worden <worden.nathan@gmail.com>
2021-08-18 06:34:33 -0700

    small updates

--- 01.Intro_to_Django.py
+++ 01.Intro_to_Django.py
@@ -1,2 +1,3 @@
 # Django provides high-level abstractions of common Web development patterns, shortcuts for frequent programming tasks, and clear conventions for how to solve problems.
 
+# Django speeds up development process
--- 05.regex.py
+++ 05.regex.py
@@ -6,4 +6,6 @@
 [A-Za-z]     Any character between a and z (case insensitive)
 +            One or more of the previous expressions (e.g. \d+ matches one or more digits)
 [^/]+        One or more characters until (and not including a forward slash)
-?            Zero or one of the previous expressions (e.g. \d+ matches one or more digits)
+?            Zero or one of the previous expressions (e.g. \d? matches zero or one digits)
+*            Zero or more of the previous expressions (e.g. \d? matches zero, one, or one digits
+{1, 3}       Between one and three (inclusive) of the previous expression (e.g., \d{1, 3} matches one, two or three digits)
