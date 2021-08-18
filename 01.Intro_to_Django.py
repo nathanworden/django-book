dd5127d5afd1766d51fecf3b43afef34e6428322
Nathan Worden <worden.nathan@gmail.com>
2021-08-18 06:38:47 -0700

    add

--- 01.Intro_to_Django.py
+++ 01.Intro_to_Django.py
@@ -1,3 +1,5 @@
 # Django provides high-level abstractions of common Web development patterns, shortcuts for frequent programming tasks, and clear conventions for how to solve problems.
 
 # Django speeds up development process
+
+#add
